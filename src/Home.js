31eadc54ea92a636af392adc4a08009ed6f3dc42
Muthu6543 <tomuthuprabucm@gmail.com>
2020-08-21 06:49:31 +1000

    first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO Farheen Galaria Here</h2>
+        <p>I am a Good Girl.</p>
+ 
+        <p>I like Books.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stuff</h2>
+        <p>Like to learn New info</p>
+        <ol>
+          <li>I like English</li>
+          
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
